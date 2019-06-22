7fb9a1051d957ec39a85e6942aca84421743d377
Dov <adovene@gmail.com>
2019-06-22 22:09:50 +0200

    Initial Commit

---
+++ TemplateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplateApp
+//
+//  Created by Prismea_Strasbourg on 22/06/2019.
+//  Copyright © 2019 Dov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
